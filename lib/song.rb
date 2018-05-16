d84872424d4c24d04765e8c27a636334c126af9f
Tony Ho <ho.tonym@gmail.com>
2018-05-16 18:18:15 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,43 @@
+class Song
+
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  @@genre_count = {}
+  @@artist_count = {}
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << self.artist
+    @@genres << self.genre
+    @@genre_count[self.genre] ||= 0
+    @@genre_count[self.genre] += 1
+    @@artist_count[self.artist] ||= 0
+    @@artist_count[self.artist] += 1
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    @@genre_count
+  end
+
+  def self.artist_count
+    @@artist_count
+  end
+end
